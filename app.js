ac0f43e2e7ad6215e76de5c6cc7403ee32398a5a
nfahmed11 <nfahmed11@gmail.com>
2022-02-07 15:23:52 -0600

    final

--- app.js
+++ app.js
@@ -111,18 +111,28 @@ console.table(yearsLived);
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
-let list = document.querySelector(".mw-category");
-let names = Array.from(list.querySelectorAll("a"));
-console.log(names);
+// uncomment and paste into console of wiki page
 
-let allnames = names
-  .map((item) => item.innerHTML)
-  .filter((a) => a.includes("de"));
+// let list = document.querySelector(".mw-category");
+// let names = Array.from(list.querySelectorAll("a"));
+// console.log(names);
 
-console.log(allnames);
+// let allnames = names
+//   .map((item) => item.innerHTML)
+//   .filter((a) => a.includes("de"));
+
+// console.log(allnames);
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+
+const alpha = people.sort(function (a, b) {
+  const [alast, afirst] = a.split(", ");
+  const [blast, bfirst] = b.split(", ");
+  return alast > blast ? 1 : -1;
+});
+
+console.log(alpha);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
@@ -142,3 +152,14 @@ const data = [
   "car",
   "truck",
 ];
+
+const transport = data.reduce(function (obj, item) {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(transport);
